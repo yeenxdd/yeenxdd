49d37be82cc463151e2c0565d7fa57d5d8066d67
yeen <yeenxdd@gmail.com>
2023-09-02 10:11:28 +0800

    Update portfolio

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ $(document).ready(function(){
 
     //typing animation script
     new Typed("#typing", {
-        strings: ["Game Designer", "Developer", "UI/UX Designer", "Motion Designer", "3D Modeller", "Creative Designer"],
+        strings: ["Game Designer", "XR Developer", "Game QA Engineer", "Motion Designer", "Unity Developer", "Creative Designer"],
         typeSpeed: 50,
         backSpeed: 60,
         loop: true
     })
     new Typed("#typing-2", {
-        strings: ["Game Designer", "Developer", "UI/UX Designer", "Motion Designer", "3D Modeller", "Creative Designer"],
+        strings: ["Game Designer", "XR Developer", "Game QA Engineer", "Motion Designer", "Unity Developer", "Creative Designer"],
         typeSpeed: 50,
         backSpeed: 60,
         loop: true
